d38eeccd821932e647b47049be4d062b0c1ba34e
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-28 10:34:02 +0530

    WORKING ON COMPONENTS IN REACT

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+import {Routes} from 'react-router-dom';
+import Login from './components/login/Login.jsx';
+import Restaurant from './components/restaurant/Restaurant.jsx';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route exact path = '/login' element = {<Login />}/>
+        <Route exact path = '/restaurant' element = {<Restaurant /> }/>}/>
+      </Routes>
+   </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/login/Login.jsx
@@ -0,0 +1,9 @@
+function Login(params) {
+    return(
+        <div>
+            <h1>Login Component</h1>
+        </div>
+    )
+}
+
+export default Login;
