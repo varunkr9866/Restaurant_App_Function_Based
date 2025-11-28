eeedf2d8ca7e5df95cdb1de165d405038227f402
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-28 16:17:01 +0530

    COMPLETED RESTAURANT DATA DISPLAY

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,38 +1,45 @@
+import { useNavigate } from 'react-router-dom';
+import './Login.css'
 
-function Login(params) {
-    function handleSubmit(event){
+function Login() {
+    const navigate = useNavigate();
+
+    async function handleSubmit(event) {
         event.preventDefault();
-        let user ={
-        username : event.target.username.value;
-        password : event.target.password.value;
-        }
+
+        let user = {
+            username: event.target.username.value,
+            password: event.target.password.value
+        };
+
         let backend_url = 'http://localhost:3200/login';
+
         let options = {
-            method:"post",
-            headers : {'content-type':'application/json'},
-            body : JSON.stringify(user)
-        }
-        let response = await fetch(backend_url,options);
-        console.log(response);
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user)
+        };
+
+        let response = await fetch(backend_url, options);
         let responseData = await response.json();
-        if(responseData.flag === true){
-            NavigationHistoryEntry('/restaurant')
-        }else{
-            alert(responseData.msg)
+
+        if (responseData.flag === true) {
+            navigate('/restaurant');
+        } else {
+            alert(responseData.msg);
         }
-        
-        
     }
-    return(
+
+    return (
         <div>
             <h1>Login Form</h1>
             <form onSubmit={handleSubmit}>
-                Username: <input type="text" name="username" id="username"></input>
-                Password: <input type="password" name="password" id="password"></input>
+                Username: <input type="text" name="username" />
+                Password: <input type="password" name="password" />
                 <input type="submit" value="login" />
             </form>
         </div>
-    )
+    );
 }
 
 export default Login;
--- frontend/src/components/restaurant/Restaurant.jsx
+++ frontend/src/components/restaurant/Restaurant.jsx
@@ -1,4 +1,23 @@
-function Restaurant(params) {
+import { useState,useEffect } from "react";
+
+function Restaurant(props) {
+    const [menuData,setMenuData] = useState({
+        flag:false,
+        foodData: null,
+        drinkData:null
+    })
+    async function getData(){
+        let backend_url='http://localhost:3200/data';
+        let response = await fetch(backend_url);
+        let responseData =awaitresponse.json();
+
+        console.log(responseData);
+        
+    }
+    useEffect(()=>{
+        getData();
+    },[]);
+    
     return(
         <div>
             <h1>Restaurant Component</h1>
