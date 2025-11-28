ac0dcc2c7eed853aee93e562e2ed5173d374d28a
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-28 16:32:35 +0530

    WORKING ON RESTARANT DATA

--- frontend/src/components/restaurant/Restaurant.jsx
+++ frontend/src/components/restaurant/Restaurant.jsx
@@ -1,28 +1,39 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function Restaurant(props) {
-    const [menuData,setMenuData] = useState({
-        flag:false,
-        foodData: null,
-        drinkData:null
-    })
-    async function getData(){
-        let backend_url='http://localhost:3200/data';
-        let response = await fetch(backend_url);
-        let responseData =awaitresponse.json();
+  const [menuData, setMenuData] = useState({
+    flag: false,
+    foodData: null,
+    drinkData: null,
+  });
 
-        console.log(responseData);
-        
-    }
-    useEffect(()=>{
-        getData();
-    },[]);
-    
-    return(
-        <div>
-            <h1>Restaurant Component</h1>
-        </div>
-    )
+  async function getData() {
+    let backend_url = "http://localhost:3200/data";
+    let response = await fetch(backend_url);
+    let responseData = await response.json();
+
+    console.log(responseData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div classname="pri">
+      <h1>Food Menu</h1>
+      {menuData.foodData}
+      <h1>Drink Menu</h1>
+      {menuData.drinkData}
+      <Copyright />
+    </div>
+  );
+
+  let arr = ["balaji", "semma", "kishan"];
+  return (
+    <div>
+      <h1>{arr}</h1>
+    </div>
+  );
 }
-
 export default Restaurant;
