649c66e37ed28119bd08fc908235828add639709
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-28 10:36:13 +0530

    WORKING ON COMPONENT RESTAURANT IN REACT

--- frontend/src/components/restaurant/Restaurant.jsx
+++ frontend/src/components/restaurant/Restaurant.jsx
@@ -1,3 +1,9 @@
-export default function Restaurant(params) {
-    
+function Restaurant(params) {
+    return(
+        <div>
+            <h1>Restaurant Component</h1>
+        </div>
+    )
 }
+
+export default Restaurant;
