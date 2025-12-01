f192c82be37e4e85da0af33f56a121a5f310b049
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-12-01 11:17:28 +0530

    MADE BASIC LOGIN COMPONENT

---
+++ frontend_class/src/components/login/Login.jsx
@@ -0,0 +1,19 @@
+import React ,{COmponent} from 'react';
+import './Demo.css';
+
+
+class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+    render(){
+        return(
+            <></>
+        )
+    }
+}
+
+export default Login;
